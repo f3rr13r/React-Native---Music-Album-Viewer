a1401989bd7ab8d491f535ca0cc3090cb3a54cf8
Harry Ferrier <f3rr13r@hotmail.com>
2016-11-23 12:26:05 +0000

    Push React-Native code from local repository to github repo

---
+++ index.ios.js
@@ -0,0 +1,22 @@
+// Index.ios.js - Place code inside of here for iOS
+
+
+// Import a library to help create a component.
+import React from 'react';
+import { View, AppRegistry } from 'react-native';
+
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+
+// Create a component.
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerTitle={'Albums'} />
+    <AlbumList />
+  </View>
+);
+
+// Render it to the device.
+AppRegistry.registerComponent('albums', () => App);
+
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+// Import libraries for making a component
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+// Make a component
+class Header extends Component {
+	render() {
+		const { headerTitle } = this.props;
+		const { viewStyle, textStyle } = styles;
+
+		return (
+			<View style={viewStyle}>
+				<Text style={textStyle}>{headerTitle}</Text>
+			</View>
+		);
+	}	
+}
+
+const styles = {
+	viewStyle: {
+		backgroundColor: '#F8F8F8',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 2
+		},
+		shadowOpacity: 0.2,
+		elevation: 2,
+		position: 'relative',
+	},
+		textStyle: {
+		fontSize: 20,
+	}
+};
+
+
+// Make the component available to other parts of the app
+
+export default Header;
